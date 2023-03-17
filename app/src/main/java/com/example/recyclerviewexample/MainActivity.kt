23e1ab46d1722cbcb55e8fcd899875a94ce1c7bc
CarlosMurillo <carlmuva@hotmail.com>
2023-03-17 17:28:29 -0600

    implementacion de viewBinding

--- app/src/main/java/com/example/recyclerviewexample/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewexample/MainActivity.kt
@@ -5,22 +5,28 @@ import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recyclerviewexample.adapter.SuperHeroAdapter
+import com.example.recyclerviewexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater) // con binding
+        setContentView(binding.root)    //con binding
+        //setContentView(R.layout.activity_main)  // sin binding
         initRecyclerView()
     }
 
 
     private fun initRecyclerView(){
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerSuperHero)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = SuperHeroAdapter(SuperHeroProvider.superHeroList)
+        //val recyclerView = findViewById<RecyclerView>(R.id.recyclerSuperHero)   Sin binding
+        //recyclerView.layoutManager = LinearLayoutManager(this)   Sin binding
+        //recyclerView.adapter = SuperHeroAdapter(SuperHeroProvider.superHeroList) Sin binding
+
+        binding.recyclerSuperHero.layoutManager=LinearLayoutManager(this)  // con binding
+        binding.recyclerSuperHero.adapter = SuperHeroAdapter(SuperHeroProvider.superHeroList) // con binding
 
 
     }
--- app/src/main/java/com/example/recyclerviewexample/adapter/SuperHeroViewHolder.kt
+++ app/src/main/java/com/example/recyclerviewexample/adapter/SuperHeroViewHolder.kt
@@ -9,24 +9,35 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.GlideException
 import com.example.recyclerviewexample.R
 import com.example.recyclerviewexample.SuperHero
+import com.example.recyclerviewexample.databinding.ItemSuperheroBinding
 
-class SuperHeroViewHolder(view:View): RecyclerView.ViewHolder(view) {
+class SuperHeroViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
-    val superHero = view.findViewById<TextView>(R.id.tvSuperHeroName)
-    val realName = view.findViewById<TextView>(R.id.tvRealName)
-    val publisher = view.findViewById<TextView>(R.id.tvPublisher)
-    val photo = view.findViewById<ImageView>(R.id.ivSuperHero)
+    val binding = ItemSuperheroBinding.bind(view)
+
+    //val superHero = view.findViewById<TextView>(R.id.tvSuperHeroName) // sin binding
+    //val realName = view.findViewById<TextView>(R.id.tvRealName) // sin binding
+    //val publisher = view.findViewById<TextView>(R.id.tvPublisher)// sin binding
+    //val photo = view.findViewById<ImageView>(R.id.ivSuperHero)// sin binding
 
 
+    fun render(superHeroModel: SuperHero) {
 
+        binding.tvSuperHeroName.text = superHeroModel.superhero
+        binding.tvRealName.text = superHeroModel.realName
+        binding.tvPublisher.text = superHeroModel.publisher
+        Glide.with(binding.ivSuperHero.context)
+            .load(superHeroModel.photo)
+            .into(binding.ivSuperHero)
 
-    fun render(superHeroModel: SuperHero){
-        superHero.text = superHeroModel.superhero
+/*
+        superHero.text = superHeroModel.superhero  sin binding
         realName.text = superHeroModel.realName
         publisher.text = superHeroModel.publisher
         Glide.with(photo.context)
             .load(superHeroModel.photo)
             .into(photo)
+*/
 
     }
 }
