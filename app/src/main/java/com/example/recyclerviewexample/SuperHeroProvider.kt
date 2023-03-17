bde43bc97330d1393d0fedcec20712bec8ecf362
CarlosMurillo <carlmuva@hotmail.com>
2023-03-17 14:55:38 -0600

    Primer commit

---
+++ app/src/main/java/com/example/recyclerviewexample/SuperHeroProvider.kt
@@ -0,0 +1,46 @@
+package com.example.recyclerviewexample
+
+class SuperHeroProvider {
+
+    companion object {
+        val superHeroList = listOf<SuperHero>(
+            SuperHero(
+                "Wolverine",
+                "X-MENS",
+                "Logan",
+                "https://depor.com/resizer/B1DvZCiZZ8c7EtkfCDNW2CgrtHI=/580x330/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/S7GD4Z7NVRBNBBWTGS2FUJEUXM.jpg"
+            ),
+            SuperHero(
+                "Ciclope",
+                "X-MENS",
+                "Scott",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQp04CRDEoGIsOueOAazFE_wiwGOb311Y7Dw&usqp=CAU"
+
+            ),
+            SuperHero(
+                "Phoenix",
+                "X-MENS",
+                "Jean Grey",
+                "https://static.wikia.nocookie.net/marveldatabase/images/4/4d/A.X.E._Judgment_Day_Vol_1_3_Women_of_A.X.E._Variant_Textless.jpg/revision/latest?cb=20220711112817"
+
+            ),
+            SuperHero(
+                "Magneto",
+                "X-MENS",
+                "Erick",
+                "https://sm.ign.com/t/ign_latam/screenshot/default/magneto_c3k1.1280.jpg"
+
+            ),
+            SuperHero(
+                "Profesor X",
+                "X-MENS",
+                "Charles Xavier",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4GDAcDb511CxREZ8G0TVsSuv9KRjrXafbLemq4E-NOLcgMMigoAfd0mUI03GlGDeZNYk&usqp=CAU"
+
+            )
+
+        )
+
+    }
+
+}
